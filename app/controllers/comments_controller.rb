8462e71bac85205f6cb60a48d9d598ac186da97d
RadoszSzymon <radoszszymon@gmail.com>
2016-05-18 17:32:57 +0000

    Added comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,5 +12,29 @@ class CommentsController < ApplicationController
        else
            render 'new'
        end
+       
+    end
+    
+    def edit
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+         
+        if @comment.update(params[:comment].permit(:comment))
+            redirect_to post_path(@post)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+         @post = Post.find(params[:post_id])
+         @comment = @post.comments.find(params[:id])
+         @comment.destroy
+         redirect_to post_path(@post)
     end
 end
